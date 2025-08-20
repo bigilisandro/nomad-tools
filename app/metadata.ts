e9ac9768f5f22b5b69df9edef5f357b0cdccd348
bigilisandro <bigilisandro@gmail.com>
2025-08-20 14:23:49 -0300

    added og image

--- app/metadata.ts
+++ app/metadata.ts
@@ -2,7 +2,7 @@ export const siteConfig = {
   name: "Digital Nomad Resources - Resources for Remote Workers and Digital Nomads",
   description: "Discover curated resources for remote workers and digital nomads to live, work, and thrive from anywhere in the world.",
   url: "https://digitalnomadresourc.es",
-  ogImage: "https://digitalnomadresourc.es/og.png",
+  ogImage: "https://digitalnomadresourc.es/images/digital-nomad-resources.png",
 }
 
 export type SiteConfig = typeof siteConfig 
